7cb81901f2b11f8679b78cd288d7b08c4b0b6355
Mayur <mayurkanade59@gmail.com>
2024-07-17 16:46:16 +0530

    The object which has been mentioned at the time of element creation consist attribute

--- 01-Inception/App.js
+++ 01-Inception/App.js
@@ -1,6 +1,10 @@
 // Created a h1 tag in the react and have 3 parameters:
-// 1. tagName 2. object 3. content for the respective tag.
-const heading = React.createElement("h1", {}, "Hello From React!");
+// 1. tagName 2. object consiste attribute for the tag 3. content for the respective tag.
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello From React!"
+);
 // Created a root where all the react code will be run
 // Root is the top most element in the React hierarchy.
 const root = ReactDOM.createRoot(document.getElementById("root"));
