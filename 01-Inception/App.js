cc624fa8e7e31da6e562c0d9ed92b8f06b1a23dd
Mayur <mayurkanade59@gmail.com>
2024-07-17 16:36:18 +0530

    The React code separated in App.js file

---
+++ 01-Inception/App.js
@@ -0,0 +1,8 @@
+// Created a h1 tag in the react and have 3 parameters:
+// 1. tagName 2. object 3. content for the respective tag.
+const heading = React.createElement("h1", {}, "Hello From React!");
+// Created a root where all the react code will be run
+// Root is the top most element in the React hierarchy.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// injected heading element into the react by using render method
+root.render(heading);
