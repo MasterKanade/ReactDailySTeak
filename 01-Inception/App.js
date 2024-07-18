981540b58bf0cb254e920dddb7183034ff0b975b
Mayur <mayurkanade59@gmail.com>
2024-07-18 18:21:08 +0530

    pkg json script change and JSX example

--- 01-Inception/App.js
+++ 01-Inception/App.js
@@ -1,22 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "Parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hi I'm h1 tag"),
-    React.createElement("h2", {}, "Hi I am h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Hi I'm h1 tag from child2"),
-    React.createElement("h2", {}, "Hi I am h2 tag from child2"),
-  ]),
-]);
+const heading = React.createElement(
+  "div",
+  { id: "heading" },
+  "Children Area in React Heading!"
+);
 
-console.log(parent);
+const jsxHeading = <h1 id="heading">JSX is clean clode 🚀🚀🚁</h1>;
+
+console.log(heading);
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
-
-// this above code becomes ugly and so hard to read this stuff.
-// therefore, the JSX comes into the picture for the same.
+root.render(jsxHeading);
